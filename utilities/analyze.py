c655a09c28b642acbdba5c8edbb6826d07ac8041
chillz-the-first <vhenganitshilidzi846@gmail.com>
2026-05-28 20:54:28 +0200

    feat: compute churn rate by tenure

--- utilities/analyze.py
+++ utilities/analyze.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def get_baseline_churn_rate(df):
     churn_counts = df["Churn"].value_counts(normalize=True)
     return churn_counts["Yes"]
@@ -7,10 +9,26 @@ def get_churn_by_contract(df):
     # contract_churn.columns = ["Contract", "Churn", "Churn_rate"]
     # contract_churn["Churn_rate"] = contract_churn["Churn_rate"].map('{:.1%}'.format)
 
-    df["Churn_binary"] = (df["Churn"] == "Yes").astype(int)
-    rate = df.groupby("Contract").agg(
-        customers=("Churn_binary", "size"),
-        churn_rate=("Churn_binary", "mean"),
+    # df["Churn_binary"] = (df["Churn"] == "Yes").astype(int)
+
+    return (
+        df.groupby("Contract").agg(
+            customers=("Churn_binary", "size"),
+            churn_rate=("Churn_binary", "mean"),
+        )
     )
 
-    return rate
+def get_churn_by_tenure(df):
+    df["Tenure_Tier"] = pd.cut(
+        df["tenure"],
+        bins=[0, 12, 24, 48, float('inf')],
+        labels=["0-1 yr", "1-2 yrs", "2-4 yrs", ">4 yrs"],
+        include_lowest=True,
+    )
+
+    return (
+        df.groupby("Tenure_Tier", observed=False).agg(
+            customers=("Churn_binary", "size"),
+            churn_rate=("Churn_binary", "mean"),
+        )
+    )
