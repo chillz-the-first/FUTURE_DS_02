86e8faa26c68525224a05167b31d10cb4ec104dc
chillz-the-first <vhenganitshilidzi846@gmail.com>
2026-05-29 19:01:02 +0200

    feat: compute lost revenue for each segment

--- utilities/analyze.py
+++ utilities/analyze.py
@@ -5,10 +5,26 @@ def get_baseline_churn_rate(df):
     return churn_counts["Yes"]
 
 def get_churn_by(df, column):
+    print(f"\nChurn rate for {column}")
+
     return (
         df.groupby(column, observed=False).agg(
             customers=("Churn_binary", "size"),
             churn_rate=("Churn_binary", "mean"),
+        )
+    )
+
+def get_lost_revenue(df, column):
+    df = df.copy()
+    df["Lost_Revenue"] = df["MonthlyCharges"] * df["Churn_binary"]
+    print(f"\nLost Revenue for {column}")
+
+    return (
+        df.groupby(column, observed=False).agg(
+            customers=("Churn_binary", "size"),
+            churn_rate=("Churn_binary", "mean"),
+            total_revenue=("MonthlyCharges", "sum"),
+            lost_revenue=("Lost_Revenue", "sum"),
         )
     )
 
