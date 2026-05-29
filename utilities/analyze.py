a5613d2bbf762929e3221afe1f6ceec9bc34e797
chillz-the-first <vhenganitshilidzi846@gmail.com>
2026-05-29 18:41:10 +0200

    refactor: compute tenure churn rate using general function

--- utilities/analyze.py
+++ utilities/analyze.py
@@ -12,17 +12,6 @@ def get_churn_by(df, column):
         )
     )
 
-def get_churn_by_tenure(df):
-    df = df.copy()
-    df["Tenure_Tier"] = pd.cut(
-        df["tenure"],
-        bins=[0, 12, 24, 48, float('inf')],
-        labels=["0-1 yr", "1-2 yrs", "2-4 yrs", ">4 yrs"],
-        include_lowest=True,
-    )
-
-    return get_churn_by(df, "Tenure_Tier")
-
 
 
 
